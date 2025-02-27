9105242886c70a0028197c8bdba57663038a85f4
shrvats <68887685+shrvats@users.noreply.github.com>
2025-02-27 20:30:08 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,13 @@
 import QuizRenderer from './components/QuizRenderer'
 
 export default function App() {
-  return <QuizRenderer />
+  return (
+    <div className="container">
+      <h1 style={{ color: '#1a237e', textAlign: 'center' }}>
+        Exam Prep Generator
+      </h1>
+      <QuizRenderer />
+    </div>
+  )
 }
+
