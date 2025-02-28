0b34d866ea3c520acdd9382074b59a72478e6c2f
shrvats <68887685+shrvats@users.noreply.github.com>
2025-02-28 14:15:53 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
+import { MathJaxProvider } from 'mathjax3-react'
 import QuizRenderer from './components/QuizRenderer'
 
 export default function App() {
   return (
-    <div className="container">
-      <h1 style={{ color: '#1a237e', textAlign: 'center' }}>
-        Exam Prep Generator
-      </h1>
-      <QuizRenderer />
-    </div>
+    <MathJaxProvider>
+      <div className="container">
+        <h1 style={{ color: '#1a237e', textAlign: 'center' }}>
+          Exam Prep Generator
+        </h1>
+        <QuizRenderer />
+      </div>
+    </MathJaxProvider>
   )
 }
+
 
