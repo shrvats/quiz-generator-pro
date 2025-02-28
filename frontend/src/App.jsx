b7d69f4acc5dd2de268e7d656dfc426edb8a6d5f
shrvats <68887685+shrvats@users.noreply.github.com>
2025-02-28 15:10:52 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,7 @@ import QuizRenderer from './components/QuizRenderer'
 
 export default function App() {
   return (
-    <MathJaxContext>
-      <div className="container">
-        <h1 style={{ color: '#1a237e', textAlign: 'center' }}>
-          Exam Prep Generator
-        </h1>
-        <QuizRenderer />
+   
       </div>
        <div className="container">
       <h1 style={{ color: 'red', textAlign: 'center' }}>
