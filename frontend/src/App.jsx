88fed313e6536b7a9ad79c010abbd1ad0ef4a26d
shrvats <68887685+shrvats@users.noreply.github.com>
2025-02-28 15:47:33 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,17 @@ import QuizRenderer from './components/QuizRenderer'
 
 export default function App() {
   return (
-   
+    <MathJaxContext>
+      <div className="container">
+        <h1 style={{ color: '#1a237e', textAlign: 'center' }}>
+          PDF Quiz Generator
+        </h1>
+        <QuizRenderer />
       </div>
-       <div className="container">
-      <h1 style={{ color: 'red', textAlign: 'center' }}>
-        TEST - If you see this, React is working
-      </h1>
-      <QuizRenderer />
-    </div>
     </MathJaxContext>
   )
 }
+
 
 
 
