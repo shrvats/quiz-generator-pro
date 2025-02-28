ebcb19c63827d1d3def24b0020445ac846f47d0e
shrvats <68887685+shrvats@users.noreply.github.com>
2025-02-28 17:56:51 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,18 @@ import os
 from typing import List, Dict
 
 app = FastAPI()
+app.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+      );
+      res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+      if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+      }
+      next();
+    });
 
 app.add_middleware(
     CORSMiddleware,
