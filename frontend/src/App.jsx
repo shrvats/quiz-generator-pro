11d2a1dcf1945304d5a7022a104caadedb1647e3
shrvats <68887685+shrvats@users.noreply.github.com>
2025-03-01 15:12:25 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,10 @@
-import React from 'react';
-import { MathJaxContext } from 'better-react-mathjax';
-
-// Import both components for testing
-import QuizRenderer from './components/QuizRenderer';
-import DebugQuizRenderer from './components/DebugQuizRenderer'; // Make sure to save the debug component with this name
-import PathFinder from './components/PathFinder';
-
-export default function App() {
-  // Current timestamp to verify this file is being used
-  const buildTime = new Date().toISOString();
-  
-  console.log("App.jsx rendered at:", buildTime);
-  
+function App() {
   return (
-    <MathJaxContext>
-      <div className="container">
-        <h1 style={{ 
-          color: 'red', // Changed to red to verify update
-          textAlign: 'center',
-          border: '3px solid blue',
-          padding: '10px',
-          margin: '10px'
-        }}>
-          PDF Quiz Generator (Debug Build: {buildTime})
-        </h1>
-        
-        {/* Path finder to help debug */}
-        <PathFinder />
-        
-        {/* Use Debug version instead of regular version */}
-        <DebugQuizRenderer />
-        
-        {/* Comment out the original component
-        <QuizRenderer />
-        */}
-      </div>
-    </MathJaxContext>
+    <div>
+      <h1 style={{color: 'red', fontSize: '40px'}}>
+        THIS IS A TEST CHANGE
+      </h1>
+      {/* Rest of your app */}
+    </div>
   );
 }
